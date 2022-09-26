d7552f2492b94f7595da747f214c6e489571c8db
Mani9257 <53720673+Mani9257@users.noreply.github.com>
2022-09-26 16:28:01 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/example/HelloWorldApplication.java
+++ src/main/java/com/example/HelloWorldApplication.java
@@ -9,4 +9,12 @@ public class HelloWorldApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
 	}
-}
+
+while(True):
+   user_in = input("").lower()
+   if(("add" in user_in)and("fix" in user_in)and("update" in user_in)):
+       print("Commit Verified")
+       break
+   else:
+       print("not verified since you are missing with any of fix or ADD or Update")
+	}
